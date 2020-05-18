0207831d25ea9d89cd4e78e06ea856b6697aef13
Cristian D. Ceballos <cdceballor@eafit.edu.co>
2020-05-18 13:42:49 -0500

    It's working? idk

--- h.py
+++ h.py
@@ -1,21 +1,34 @@
-from scipy.linalg import hadamard
-import math as mt
-from sympy.physics.quantum.gate import H
-from sympy.physics.quantum.gate import CNOT
-from sympy.physics.quantum.gate import CNotGate
-from sympy.physics.quantum.gate import HadamardGate
+import numpy as np
 
 
-escalar = 1. / (mt.sqrt(2.))
-p = hadamard(4)
+## Creamos estado inicial, dos qubit en |0>, es decir |00>
+zero = np.array([1,0]).reshape(2,1)
+zero_zero = np.kron(zero,zero)
 
-"""
-print("Hadamard ")
-print(p)
+## Aplicamos H al primer cubit del sistema
+## Dado que tenemos un sistema de 2 qubits, tenemos que hacer un cambio en H
+i = 1/np.sqrt(2)
+h_1 = np.array([[i,i],[i,-i]])
+h_2 = np.kron(h_1,np.eye(2))
 
-print("Hadamard por escalar")
-print(p*escalar)
+## operamos al primer estado
+t1 = h_2 @ zero_zero
 
-print("Hadamard dividido escalar")
-print(p/escalar)
-"""
+## Aplicamos la defincion de CX con el control en el primer qubit y el X en el segundo
+X = np.array([[0,1],[1,0]])
+C_1_X_2 = np.array([[1,0,0,0],[0,1,0,0],[0,0,0,1],[0,0,1,0]])
+C_2_X_1 = np.array([[1,0,0,0],[0,0,0,1],[0,0,1,0],[0,1,0,0]])
+
+## Operamos al segundo estado
+
+t2 = C_1_X_2 @ t1
+
+### Operamos al tercer estado con otro H
+
+t3 = h_2 @ t2
+
+## esto deberia dar (0.5,0.5,0.5,-0.5), miren en qiskit
+print(t3)
+
+a = 9
+o = 2
--- teleportation.py
+++ teleportation.py
@@ -3,8 +3,11 @@ from scipy.linalg import hadamard
 import math as mt
 
 def Hadamart(A):
-    print(hadamard(4))
-    """
+    print("Salida Hadamart")
+    escalar = 1 / (mt.sqrt(2))
+    print(hadamard(4)*escalar)
+    print()
+"""
     sol = A
 
     escalar = 1 / (mt.sqrt(2))
@@ -19,14 +22,16 @@ def Hadamart(A):
 def Cnot(A):
     #other_matrix = np.array([[1, 1, 1, 1], [0, 0, 0, 0], [1, 1, 1, 1], [0, 0, 0, 0]])
     cnot = np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 0, 1], [0, 0, 1, 0]])
-
+    print("Salida de la CNOT")
     print(np.dot(A, cnot))
+    print()
 
 
 def makeOperation():
-    A = np.array([[1.0,2.0,9.0,1.0],[3.0,4.0,3.0,35.0],[5.0,6.0,8.0,87.5],[2.0,7.0,9.0,32.2]])
+    A = np.array([[1.0,2.0,3.0,4.0],[1.0,2.0,3.0,4.0],[5.0,6.0,8.0,87.5],[2.0,7.0,9.0,32.2]])
 
     Hadamart(A)
     Cnot(A)
+
 
 makeOperation()
